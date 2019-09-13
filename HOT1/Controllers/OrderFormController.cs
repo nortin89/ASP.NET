5b92e50b3c2ba3846b567e8fa6de4bb04cb34975
Scott Nortin <scott_nortin@insideranken.org>
2019-09-13 12:28:32 -0500

    added if else

--- HOT1/Controllers/OrderFormController.cs
+++ HOT1/Controllers/OrderFormController.cs
@@ -18,12 +18,24 @@ namespace HOT1.Controllers
     [HttpPost]
     public ActionResult Index(OrderForm order)
     {
-      ViewBag.Quantity = order.Quantity;
-      ViewBag.PricePerShirt = 15;
-      ViewBag.Subtotal = (order.Quantity)*15;
-      ViewBag.Tax = ViewBag.Subtotal * .08;
-      ViewBag.Total = (ViewBag.Tax + ViewBag.Subtotal);
-      return View("Index");
+
+      if(order.DiscountCode == null)
+      {
+        ViewBag.Quantity = order.Quantity;
+        ViewBag.PricePerShirt = 15;
+        ViewBag.Subtotal = (order.Quantity) * ViewBag.PricePerShirt;
+        ViewBag.Tax = ViewBag.Subtotal * .08;
+        ViewBag.Total = (ViewBag.Tax + ViewBag.Subtotal);
+      }
+      else if (order.DiscountCode.ToLower() == "BB88")
+      {
+        order.Total = order.Total - Convert.ToSingle(order.Total * .20);
+      }
+      else if (order.DiscountCode.ToLower() == "CC99")
+      {
+        order.Total = order.Total - Convert.ToSingle(order.Total * .10);
+      }
+            return View("Index");
     }
     }
 }
